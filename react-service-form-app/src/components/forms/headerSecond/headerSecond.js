9d40516d9fc8013fa32d4ac7df57616951f623f6
furkancak1r <furkancakr7@gmail.com>
2023-11-27 17:39:13 +0300

    26. Commit - 27.11.2023
    Service No area is added.

--- react-service-form-app/src/components/forms/headerSecond/headerSecond.js
+++ react-service-form-app/src/components/forms/headerSecond/headerSecond.js
@@ -1,44 +1,71 @@
 import React, { useState, useEffect } from "react";
 import "./headerSecond.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useFormData } from "../../../contexts/formDataContext/formDataContext";
 
 export default function HeaderSecond() {
   const [todaysDate, setTodaysDate] = useState("");
+  const { formData, FormDataFn } = useFormData();
+  const [reportNo, setReportNo] = useState("");
+  const [serviceNo, setServiceNo] = useState("");
 
   useEffect(() => {
+    const reportDate = "reportDate";
+    const reportNo = "reportNo";
+    const serviceNo = "serviceNo";
     const getFormattedDate = () => {
       const today = new Date();
       const day = String(today.getDate()).padStart(2, "0");
       const month = String(today.getMonth() + 1).padStart(2, "0");
       const year = today.getFullYear();
       setTodaysDate(`${day}/${month}/${year}`);
+      FormDataFn({ ...formData, [reportDate]: `${day}/${month}/${year}` });
+      FormDataFn({ ...formData, [reportNo]: "1234567" });
+      FormDataFn({ ...formData, [serviceNo]: "SRV1234567" });
     };
 
+
+    setReportNo("1234567");
+    setServiceNo("SRV1234567");
     getFormattedDate();
+    // eslint-disable-next-line
   }, []);
+
   const getHeaderFirstRowClassName = () => {
     if (window.innerWidth <= 620) {
-      return "header-second-first-row box-borders-bottom row d-flex justify-content-center align-items-center";
+      return "header-second-first-row row d-flex justify-content-center align-items-center ";
     } else {
       return "header-second-first-row box-borders-left box-borders-bottom row d-flex justify-content-center align-items-center";
     }
   };
   const getHeaderSecondRowClassName = () => {
     if (window.innerWidth <= 620) {
-      return "header-second-second-row row d-flex justify-content-center align-items-center";
+      return "header-second-second-row row d-flex justify-content-center align-items-center mt-3";
     } else {
       return "header-second-second-row box-borders-left row d-flex justify-content-center align-items-center";
+    }
+  };
+  const getReportNoContainer = () => {
+    if (window.innerWidth <= 620) {
+      return "report-no-container col-md-6 d-flex justify-content-center align-items-center";
+    } else {
+      return "report-no-container col-md-6 d-flex justify-content-center align-items-center box-borders-right";
     }
   };
   return (
     <div className="header-second-container container box-borders mt-3">
       <div className="row-container row">
-        <div className="col-md-6 d-flex justify-content-center align-items-center">
-          <h2 className=" text-center">SERVİS RAPORU</h2>
+        <div className="col-md-6 d-flex justify-content-center align-items-center mt-3">
+          <h2 className="text-center">SERVİS RAPORU</h2>
         </div>
         <div className="col-md-6">
           <div className={getHeaderFirstRowClassName()}>
-            <h5 className="text-center">RAPOR NO: 1234567</h5>
+            <div className={getReportNoContainer()}>
+              <h5 className="text-center">RAPOR NO: {reportNo}</h5>
+            </div>
+            <div className="col-md-6 d-flex justify-content-center align-items-center">
+              <h5 className="text-center">SERVİS NO: {serviceNo}</h5>
+            </div>
           </div>
           <div className={getHeaderSecondRowClassName()}>
             <h5 className="text-center">Tarih: {todaysDate}</h5>
